953039e76b021d4ad21cc1275a803043c79b8c7c
gabrielmacus <gabrielmacus@gmail.com>
2018-05-16 01:28:21 -0300

    ...

--- routes/person.js
+++ routes/person.js
@@ -5,7 +5,61 @@ module.exports=
 
         demo:function (req,res,next) {
 
-            res.json({"demo":true});
+            var Package = require('../models/Package');
+
+            Package.aggregate([
+                {
+                    $lookup:
+                        {
+
+                            from: "client",
+                            pipeline: [
+                                { $match:
+                                    { $expr:
+                                        {
+                                            $eq: [ "$client",  "$_id" ]
+                                        }
+                                    }
+                                }
+
+                            ],
+                            as: "clients"
+
+                        }
+                }
+            ]).exec(function (err,results) {
+
+                console.log(err);
+                res.json(results);
+            });
+
+            /*
+            Package.aggregate([
+                {
+                    $lookup:
+                        {
+
+                            from: "clients",
+                            pipeline: [
+                                { $match:
+                                    { $expr:
+                                        {
+                                            $eq: [ "$client",  "$_id" ]
+                                        }
+                                    }
+                                },
+
+                            ],
+                            as: "clients"
+
+                        }
+                }
+            ]).exec(function (err,results) {
+
+                console.log(err);
+                res.json(results);
+            });
+            */
 
         },
         assignments:function (req,res,next) {
